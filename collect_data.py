6ee33e550eff980bc72037c7420d60c24bedf99c
Matthew <mkwlee89@gmail.com>
2023-08-04 13:03:44 -0700

    condensed collection to one py file

--- collect_data.py
+++ collect_data.py
@@ -1,7 +1,26 @@
 import pandas
 import pandas as pd
-import string
+import os
 from statsmodels.nonparametric.smoothers_lowess import lowess
+
+
+def convert():
+    did_convert = False
+    file_names = ['title.basics', 'title.ratings', 'title.crew', 'name.basics']
+    for name in file_names:
+        if not os.path.isfile('raw-csv-data/' + name + '.csv.gz'):
+            if not did_convert:
+                did_convert = True
+                print(' <some data not found>')
+
+            print(' <' + name + '> not found: converting')
+            (pd.read_csv('raw-tsv-data/' + name + '.tsv.gz', sep='\t', low_memory=False)).to_csv(
+                'raw-csv-data/' + name + '.csv.gz', index=False, compression='gzip')
+            print(' ~converted <' + name + '> to csv')
+
+    if not did_convert:
+        print(' <all data found>')
+    return
 
 
 def replace_directors(directors):
@@ -14,7 +33,7 @@ def replace_directors(directors):
 
 def collect():
     # Read basic data & only have movie data
-    titleBasics = pd.read_csv('title.basics.csv.gz', index_col=0, low_memory=False, compression='gzip')
+    titleBasics = pd.read_csv('raw-csv-data/title.basics.csv.gz', index_col=0, low_memory=False, compression='gzip')
     titleBasics = titleBasics[titleBasics['titleType'] == 'movie']
 
     # Remove movies with unknown release year, runtime, and genre
@@ -33,12 +52,12 @@ def collect():
     titleBasics = titleBasics.drop(['titleType', 'originalTitle', 'isAdult', 'endYear'], axis=1)
 
     # Import and join ratings data
-    titleRatings = pd.read_csv('title.ratings.csv.gz', index_col=0, compression='gzip')
+    titleRatings = pd.read_csv('raw-csv-data/title.ratings.csv.gz', index_col=0, compression='gzip')
     titleRatings = titleRatings[titleRatings['numVotes'] > 49]
     movies = titleBasics.join(titleRatings)
 
     # Import and join director data
-    titleCrew = pd.read_csv('title.crew.csv.gz', index_col=0, compression='gzip')
+    titleCrew = pd.read_csv('raw-csv-data/title.crew.csv.gz', index_col=0, compression='gzip')
     titleCrew = titleCrew.drop(['writers'], axis=1)
     movies = movies.join(titleCrew)
     movies = movies[movies['directors'] != '\\N']
@@ -60,7 +79,21 @@ def collect():
     movies90 = movies.sample(frac=0.9, random_state=1)
     movies10 = movies.drop(movies90.index)
 
-    movies90.to_csv('movies90.csv.gz', compression='gzip')
-    movies10.to_csv('movies10.csv.gz', compression='gzip')
-    filter_df.to_csv('loess_year_by_rating.csv.gz', compression='gzip', header=None)
-    return movies90, movies10, filter_df
+    movies90.to_csv('movies90.csv')
+    movies10.to_csv('movies10.csv')
+    filter_df.to_csv('loess_year_by_rating.csv', header=None)
+    return
+
+def main():
+    print('<Checking if tsv files need to be converted>')
+    convert()
+
+    print('<Checking if filtered & 90/10 split data exists>')
+    if not os.path.isfile('movies90.csv') or not os.path.isfile('movies10.csv') or not os.path.isfile('loess_year_by_rating.csv'):
+            print(' <collecting data into two 90/10 subsets & producing loess yearBYrating data>')
+            collect()
+    else:
+        print(' <data found>')
+
+if __name__ == '__main__':
+    main()
